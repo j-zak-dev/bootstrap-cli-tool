f87f1e971e2ba79d7dd6c3b994ec64f80b5d3349
Jakub Zak <j.zak19@unimail.winchester.ac.uk>
2026-04-24 14:34:26 +0100

    updated command descriptions

--- cmd/createGitignore.go
+++ cmd/createGitignore.go
@@ -14,14 +14,9 @@ import (
 // createGitignoreCmd represents the createGitignore command
 var excludeFile string
 var createGitignoreCmd = &cobra.Command{
-	Use:   "createGitignore",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "createGitignore",
+	Short:   "Create a new .gitignore file",
+	Long:    `Create a new .gitignore file with the option to pass in a file path with a pre-configured template for the .gitignore file.`,
 	Aliases: []string{"c-gi"},
 	Run: func(cmd *cobra.Command, args []string) {
 
--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "myapp",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "bootstrap-cli-tool, a tool for quick project scaffolding!",
+	Long:  `Use bct for quickly applying industry standard software design patters and scaffolds following well-known architectures like DDD, TDD and BDD! The various CLI commands can be used to create singular files or full project scaffolds.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
